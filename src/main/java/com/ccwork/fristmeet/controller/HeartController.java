53ae14c558db3575945c41aafcf188ae96253f6e
Charles-Cchen <920095705@qq.com>
2020-03-20 23:51:52 +0800

    我又加了一些东西

--- src/main/java/com/ccwork/fristmeet/controller/HeartController.java
+++ src/main/java/com/ccwork/fristmeet/controller/HeartController.java
@@ -1,10 +1,12 @@
 package com.ccwork.fristmeet.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/heart")
 public class HeartController {
     //我在这边加一个备注先
+    @PostMapping("/heart")
     private void heart(){
 
     }
