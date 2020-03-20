f2b6cdc39d925d2c79f51e1f7a34a93d609970c3
Charles-Cchen <920095705@qq.com>
2020-03-20 23:36:28 +0800

    我试试能不能提交

--- src/main/java/com/ccwork/fristmeet/controller/HeartController.java
+++ src/main/java/com/ccwork/fristmeet/controller/HeartController.java
@@ -1,4 +1,11 @@
 package com.ccwork.fristmeet.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HeartController {
+    //我在这边加一个备注先
+    private void heart(){
+
+    }
 }
